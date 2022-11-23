1e59b759a912735b890199034be8597abf53711d
almog <almog@quatro-digital.com>
2022-11-23 07:47:36 +0200

    style

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const app = document.querySelector("#app")
 
 setTimeout(() => {
   app.textContent = "Js triggered!"
+  app.style.background = "red"
   window.postMessage("Hello from window!")
-}, 5000);
+}, 5000)
